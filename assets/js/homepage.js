ab4d14fd879c69c90e568872b3c23a7d95c8d81b
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-06-25 12:18:56 -0500

    fetch request to github

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // formate github api 
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
 
+    // make request to the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+console.log("outside");
 getUserRepos();
