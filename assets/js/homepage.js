7b15e15b81d5b459e62e852a5eee9600eac63f93
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-06-26 14:37:59 -0500

    saving progress lesson 2

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // formate github api 
     var apiURL = "https://api.github.com/users/" + user + "/repos";
@@ -9,5 +12,20 @@ var getUserRepos = function(user) {
         });
     });
 };
-console.log("outside");
-getUserRepos();
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
